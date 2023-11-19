a55ce393a88f44ce24fb7fc86d23a906f632939f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 17:49:13 +0330

    Constants

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// Before ES6, we used the 'var' keyword to define variables, but it had some issues. Now, we prefer using 'let' and 'const' keywords.
+// let interestRate = 0.3;
+// interestRate = 1;
 
-let name;
-console.log(name); // undefined - Variables are initially assigned the value 'undefined' by default when declared.
+const interestRate = 0.3;
+// interestRate = 1; we got runtime error
+console.log(interestRate);
 
-let name2 = "Zahra";
-console.log(name2);
-
-// Rules for naming variables:
-// - Cannot be a reserved keyword like if, let, else, where, ...
-// - Should have meaningful and descriptive names (avoid names like x, a, b, ...)
-// - Cannot start with a number, e.g., 1name
-// - Cannot contain a space or hyphen (-)
-// - Should follow camel notation, e.g., firstName
-// - Are case-sensitive
-// - Each variable should be defined on a single line.
+// In situations where we do not want the value of a variable to change, we use constants instead of variables. Constants prevent reassignment of values.
+// As a best practice, if we do not need to reassign, constants should be our default choice. However, if we need to reassign, we should use 'let'.
