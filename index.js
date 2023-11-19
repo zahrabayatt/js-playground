6eac5b6bfb8272d4f46f0022c142b12ffcb65bb7
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 18:08:50 +0330

    Reference types

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// There are two types of programming languages:
-// - Static (Statically-typed): In these languages, we declare the type of a variable, and once set, it cannot change in the future.
-// - Dynamic (Dynamically-typed): In these languages, the type of variables can change during runtime.
-
-// JavaScript is a dynamic language.
-
-let name = "Zahra";
-console.log(typeof name); // string
-name = 1;
-console.log(typeof name); // number
-let firstName = undefined;
-console.log(typeof firstName); // undefined - the type and value of this variable is undefined.
-let selectedColor = null;
-console.log(typeof selectedColor); // object
+// Reference types:
+// - Object
+// - Array
+// - Function
