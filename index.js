bffa32d75a3a6088c0e1dd54b718a6b2d351f50e
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 17:39:16 +0330

    Variables

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+// Before ES6, we used the 'var' keyword to define variables, but it had some issues. Now, we prefer using 'let' and 'const' keywords.
+
+let name;
+console.log(name); // undefined - Variables are initially assigned the value 'undefined' by default when declared.
+
+let name2 = "Zahra";
+console.log(name2);
+
+// Rules for naming variables:
+// - Cannot be a reserved keyword like if, let, else, where, ...
+// - Should have meaningful and descriptive names (avoid names like x, a, b, ...)
+// - Cannot start with a number, e.g., 1name
+// - Cannot contain a space or hyphen (-)
+// - Should follow camel notation, e.g., firstName
+// - Are case-sensitive
+// - Each variable should be defined on a single line.
