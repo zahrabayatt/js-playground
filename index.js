a51c32125aed768bb42f76a15b836c0955680b0d
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 20:06:05 +0330

    Arithmetic operators

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-// In JavaScript, we have various operators. These operators are used in conjunction with variables and constants to create expressions, and through these expressions, we implement algorithms and logic.
+// We use arithmetic operators to perform calculations, similar to the calculations in mathematics.
 
-// Types of operators:
-// - Arithmetic
-// - Assignment
-// - Comparison
-// - Logical
-// - Bitwise
+console.log(x + y); // Addition operator
+console.log(x - y); // Subtraction operator
+console.log(x * y); // Multiplication operator
+console.log(x / y); // Division operator
+console.log(x % y); // Remainder operator
+console.log(x ** y); // Exponentiation operator - x to the power of y
+
+// Increment operator (++)
+console.log(++x); // 11 - first increment x and then log x in console
+
+console.log(x++); // 10 - first log x in console and then increment x
+console.log(x); // 11
+
+// Decrement operator (--)
+console.log(--x); // 9 - first decrement x and then log x in console
+
+console.log(x--); // 10 - first log x in console and then decrement x
+console.log(x); // 9
