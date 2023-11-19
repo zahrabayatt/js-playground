a7db65dd7cc69747189047da452d6238880fd415
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 17:57:00 +0330

    Primitive types

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// let interestRate = 0.3;
-// interestRate = 1;
+// In general, we have two categories of types:
+// - Primitives / Value Types
+// - Reference Types
 
-const interestRate = 0.3;
-// interestRate = 1; we got runtime error
-console.log(interestRate);
-
-// In situations where we do not want the value of a variable to change, we use constants instead of variables. Constants prevent reassignment of values.
-// As a best practice, if we do not need to reassign, constants should be our default choice. However, if we need to reassign, we should use 'let'.
+// Primitives / Value Types:
+// - String
+let name = "Zahra"; // String Literal
+// - Number
+let age = 23; // Number Literal
+// - Boolean
+let isApproved = false; // Boolean Literal
+// - undefined
+let firstName;
+let lastName = undefined;
+// - null
+let selectedColor = null;
+// - Symbol (added in ES6)
