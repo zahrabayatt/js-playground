b11f8768d836a49bc39c879446850ae760f16a3f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 23:37:09 +0330

    For...of

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-// we use for-in loop to iterate in properties of object or elements of array;
-
-const person = {
-  name: "Zahra",
-  age: 30,
-};
-
-for (const k in person) {
-  console.log(k, person[k]);
-}
-
-for (const key in person) {
-  if (person.hasOwnProperty.call(person, key)) {
-    const element = person[key];
-    console.log(element);
-  }
-}
-
-// While it is possible to iterate over an array using a 'for...in' loop,
+// While it is possible to iterate over an array using a regular for loop,
 // using the 'for...of' loop is considered a more convenient and idiomatic approach.
 
-const colors = ["red", "blue", "green"];
+const colors = ["red", "green", "blue"];
 
-for (let index in colors) {
-  console.log(index, colors[index]);
+for (const color of colors) {
+  console.log(color);
 }
