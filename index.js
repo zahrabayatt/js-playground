9b958f55eb61b82a79b822aa0506e56fe38c6f5b
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 23:52:07 +0330

    Exercise - Landscape or portrait

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-console.log(max(1, 2));
-
-function max(a, b) {
-  //   if (a > b) return a;
-  //   return b;
-  return a > b ? a : b;
+console.log(isLandscape(400, 300));
+function isLandscape(width, height) {
+  return width > height;
 }
