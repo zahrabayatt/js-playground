0266c71ede467dda7711beae3a8e7e8661393439
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 23:25:57 +0330

    Infinite loops

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let i = 0;
-do {
-  if (i % 2 !== 0) console.log(i);
-  i++;
-} while (i <= 5);
+while (true) {
+  console.log("Hi");
+}
 
-// do-while always executed at least once.
+let i = 0;
+while (i < 5) {
+  console.log("hi");
+}
+
+do {} while (true);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//Avoid infinite loops because it can crashes your browsers or computers.
