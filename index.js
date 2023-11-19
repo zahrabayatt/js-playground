5ea11085d73ffb6595e10c2d2815309e61a6ce25
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 23:17:45 +0330

    While

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-let array = [1, 2, 3, 4];
+let i = 0;
 
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  console.log(element);
-}
-
-for (let i = 0; i < 5; i++) {
+while (i <= 5) {
   if (i % 2 !== 0) console.log(i);
+  i++;
 }
