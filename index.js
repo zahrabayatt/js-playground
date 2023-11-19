4e39dae2aa5e1eeed3a69db9d39006a61deab686
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 21:09:26 +0330

    Operator precedence

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-// 8 bit = 1 Bite
-// Decimal = Binary
-// 1 = 00000001
-// 2 = 00000010
+let x = 2 + 3 * 4;
+console.log(x); // 14
 
-// Bitwise OR operator
-console.log(1 | 2); // 3 - which equals to 00000011
-
-// Bitwise AND operator
-console.log(1 & 2); // 0 - which equals to 00000000
-
-// Real world example:
-// Read, Write, Execute
-// 00000100 --> has only read permission
-// 00000110 --> has read and write permission
-// 00000111 --> has all permissions
-
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
-
-let message = myPermission & readPermission ? "yes" : "no";
-
-console.log(message);
+let y = (2 + 3) * 4;
+console.log(y); // 20
