276c66f5e2cb2b4b3d260c05fb086b22329a29ef
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-19 19:40:55 +0330

    Functions

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-let selectedColors = ["red", "blue"]; // Array Literal
-console.log(selectedColors); // {'red', 'blue'}
-console.log(selectedColors[0]); // red
+// functions are set of statement that performance the task or calculate a value.
+// functions declaration:
+function great(name, lastName) {
+  // name, lastName are parameter of great function
+  console.log("Hi " + name + " " + lastName);
+}
 
-selectedColors[2] = "green";
-console.log(selectedColors);
-
-selectedColors[2] = 1; // we can change type of element in arrays because JS is Dynamic language
-console.log(selectedColors);
-
-// Technically, an array is an object. For example, the 'length' property comes from the object nature of arrays.
-console.log(selectedColors.length);
+great("Zahra"); // 'Zahra' is argument of great function
+great("John", "Smith");
