82876e45f74b194a3ea37120cb7ecf1c4eb0003f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-20 13:09:37 +0330

    Basics (OOP, method, object)

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-showPrimes(23);
+// Objects are collections of key-value pairs. If we have properties that are highly related, we encapsulate them in an object.
 
-function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++)
-    if (isPrime(number)) console.log(number);
-}
+// We can group these variables into one object.
+let radius = 1;
+let x = 1;
+let y = 1;
 
-function isPrime(number) {
-  for (let factor = 2; factor < number; factor++)
-    if (number % factor === 0) return false;
+// Object-oriented Programming (OOP): It is the style of programming where we view a program as a collection of objects that communicate with each other to perform some functionality.
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
 
-  return true;
-}
+  // We can group related functions in an object, and we call these instances of functions methods.
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+// Call the draw method of the circle object.
+circle.draw();
