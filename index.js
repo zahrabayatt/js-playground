3e26b94701802f91ca7f4c813c59ef957f87633f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-20 17:00:12 +0330

    Dynamic nature of objects

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Constructor Function: It is another approach for creating or constructing objects to place all the logic of the object in a single place.
+// In JavaScript, objects are dynamic, which means that once we create them, we can edit properties or methods of objects, or add or remove properties or methods.
 
-// We use Camel notation for factory functions, but we use Pascal notation for constructor functions.
+const circle = {
+  radius: 1,
+};
 
-// Constructor Function
-function Circle(radius) {
-  this.radius = radius; // add a new property
-  this.draw = function () {
-    console.log("draw");
-  };
-}
+// Add color property
+circle.color = "yellow";
+// Add draw method
+circle.draw = function () {
+  console.log("draw");
+};
 
-// Create an object from the constructor function
-const circle = new Circle();
+// Remove color property
+delete circle.color;
+// Remove draw method
+delete circle.draw;
 
-// When we use the 'new' keyword for creating an object:
-// 1 - const x = {}; creates an empty object
-// 2 - 'this' refers to the object that is created
-// 3 - the object is implicitly returned from the Circle function
+console.log(circle);
