03b12bbe971f7a025b1b5a09b328f605ed6a74b1
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-20 12:58:03 +0330

    Exercise - Prime numbers

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-showStars(5);
+showPrimes(23);
 
-function showStars(rows) {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = "";
-    for (let i = 0; i < row; i++) pattern += "*";
-    console.log(pattern);
-  }
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
 }
