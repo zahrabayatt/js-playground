cca8e1c8e745f2b6366b34a3fa3c007d5b8801b6
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-20 12:38:59 +0330

    Exercise - Starts

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const marks = [80, 80, 50];
+showStars(5);
 
-console.log(calculateGrade(marks));
-
-function calculateGrade(marks) {
-  const average = calculateAverage(marks);
-  return mapAverageToGrade(average);
-}
-
-function calculateAverage(array) {
-  let sum = 0;
-  for (const value of array) sum += value;
-  return sum / array.length;
-}
-
-function mapAverageToGrade(average) {
-  if (average < 60) return "F";
-  if (average < 70) return "D";
-  if (average < 80) return "C";
-  if (average < 90) return "B";
-  return "A";
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+    let pattern = "";
+    for (let i = 0; i < row; i++) pattern += "*";
+    console.log(pattern);
+  }
 }
