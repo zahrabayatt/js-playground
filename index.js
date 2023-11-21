f2541df6bd087211f833473f36b972363e14d38b
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 22:56:47 +0330

    Getters and setters

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-function interest(principal, rate = 3.5, years = 5) {
-  return ((principal * rate) / 100) * years;
-}
+const p = {
+  firstName: "Zahra",
+  lastName: "Bayat",
+};
 
-console.log(interest(1000));
+console.log(`${p.firstName} ${p.lastName}`);
 
-// for best practice default parameters should be last parameters
+// we may use full name in a lot of code so it is better to move it in object as method
+
+const person = {
+  firstName: "Zahra",
+  lastName: "Bayat",
+  fullName() {
+    // short syntax of fullName: function() {}
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+console.log(person.fullName());
+
+// we can't use fullName like this:
+// person.fullName;
+// person.fullName = 'John Smith';
+
+// in this situation we can use getter and setter
+
+const another = {
+  firstName: "Zahra",
+  lastName: "Bayat",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+
+  set fullName(value) {
+    const parts = value.split(" ");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+another.fullName = "Sara Rad";
+
+console.log(another.fullName);
