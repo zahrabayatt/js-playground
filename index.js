acff5920fffad96a2028dab625bdfe3eba21d7d6
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 18:49:27 +0330

    Filtering an array

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const numbers = [1, -1, 2, 3];
 
-// as soon as get false from element it going to return false and doesn't check all members
-const allPositive = numbers.every((value) => value >= 0);
+const filtered = numbers.filter((n) => n >= 0);
 
-// as soon as get true from element it going to return true and doesn't check all members
-const atLeastOnePositive = numbers.some((value) => value >= 0);
-
-console.log(allPositive);
-console.log(atLeastOnePositive);
-
-// some and every are new method which might some browsers don't support them.
+console.log(filtered);
