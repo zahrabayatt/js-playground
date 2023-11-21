79a39025fc7c39bf9fbc810c2ba480d973c09f24
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 17:23:12 +0330

    Spread operator

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-// Primitives - copy by value
-
 const first = [1, 2, 3];
 const second = [4, 5, 6];
 
-// combined
-const combined = first.concat(second);
+// const combined = [...first, ...second];
+const combined = [...first, "a", ...second, "b"]; // with spread operator we have more flexibility
 
-// slice
-
-const copy = combined.slice();
-
-const sliceFromIndex = combined.slice(2);
-
-const sliceRageIndex = combined.slice(1, 4);
+const copy = [...combined];
 
 console.log(combined);
 console.log(copy);
-console.log(sliceFromIndex);
-console.log(sliceRageIndex);
 
-// References - copy by reference
-const obj = [{ id: 10 }];
-const another = [1, 2, 3];
+const a = [{ id: 12 }];
+const b = [...a];
+a[0].id = 13;
 
-const c = obj.concat(another);
-
-const s = c.slice();
-
-obj[0].id = 12;
-
-console.log(obj);
-console.log(c);
-console.log(s);
+console.log(a);
+console.log(b);
