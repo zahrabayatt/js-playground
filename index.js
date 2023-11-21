4ca2c1b0686e9bdcb463283f4d02fd70d1271d21
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 23:21:07 +0330

    Try and catch

--- index.js
+++ index.js
@@ -27,17 +27,21 @@ console.log(person.fullName());
 const another = {
   firstName: "Zahra",
   lastName: "Bayat",
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-
   set fullName(value) {
+    if (typeof value !== "string") throw new Error("Value is not a string.");
+
     const parts = value.split(" ");
+    if (parts.length !== 2) throw new Error("Enter a first and last name.");
+
     this.firstName = parts[0];
     this.lastName = parts[1];
   },
 };
 
-another.fullName = "Sara Rad";
+try {
+  another.fullName = "";
+} catch (error) {
+  alert(error.message);
+}
 
 console.log(another.fullName);
