0e3939b387c16ebb55017185c0470d55cdadb3d0
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 21:35:12 +0330

    Function declaration vs expression

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const movies = [
-  { title: "a", year: 2023, rating: 4.5 },
-  { title: "b", year: 2023, rating: 4.7 },
-  { title: "c", year: 2023, rating: 3 },
-  { title: "d", year: 2014, rating: 4.5 },
-];
+// In JS, we have two way to defining functions:
+// 1- Function Decurion
+// 2- Function Expression
 
-const output = movies
-  .filter((m) => m.year === 2023 && m.rating >= 4)
-  //   .sort((a, b) => {
-  //     // descending sort
-  //     if (a.rating < b.rating) return 1;
-  //     if (a.rating > b.rating) return -1;
-  //     return 0;
-  //   })
-  .sort((a, b) => b.rating - a.rating) // descending sort - both rating are number so we can use short way
-  .map((obj) => obj.title);
+// Function Declaration
+function walk() {
+  console.log("walk");
+}
 
-console.log(output);
+walk();
+
+// Function Expression
+let run = function run() {
+  console.log("run");
+};
+
+// Anonymous Function Expression
+run = function () {
+  console.log("run faster");
+};
+
+let move = run;
+
+run();
+move();
