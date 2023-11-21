900b9bf8a23e830f47ad36c7993af23281c8c8c4
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 12:31:51 +0330

    Exercise - Object equality

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-// const address = createAddress("a", "b", "c");
-const address = new Address("a", "b", "c");
-console.log(address);
+const address1 = new Address("a", "b", "c");
+const address2 = new Address("a", "b", "c");
+address2.color = "red";
 
-// Factory Function
-function createAddress(street, city, zipCode) {
-  return {
-    street,
-    city,
-    zipCode,
-  };
-}
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
 
 // Constructor Function
 function Address(street, city, zipCode) {
   this.street = street;
   this.city = city;
   this.zipCode = zipCode;
+}
+
+function areEqual(address1, address2) {
+  if (getNumberOfProperty(address1) !== getNumberOfProperty(address2))
+    return false;
+  for (const key in address1) {
+    if (!(key in address2) || address1[key] !== address2[key]) return false;
+  }
+
+  return true;
+}
+
+function getNumberOfProperty(obj) {
+  return Object.keys(obj).length;
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
 }
