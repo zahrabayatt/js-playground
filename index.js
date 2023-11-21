6feb6e118b2cfcd07172e0b4f8e4cb55ade50c4f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 17:26:19 +0330

    Iterating an array

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+const numbers = [1, 2, 3];
 
-// const combined = [...first, ...second];
-const combined = [...first, "a", ...second, "b"]; // with spread operator we have more flexibility
+// we don't get index with for-of loop
+for (const number of numbers) {
+  console.log(number);
+}
 
-const copy = [...combined];
+// numbers.forEach(function (number) {
+//   console.log(number);
+// });
+numbers.forEach((number) => console.log(number));
 
-console.log(combined);
-console.log(copy);
-
-const a = [{ id: 12 }];
-const b = [...a];
-a[0].id = 13;
-
-console.log(a);
-console.log(b);
+// we get index with forEach loop
+numbers.forEach((number, index) => console.log(number, number));
