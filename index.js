ff8faef4da88befbe316aa5a006dcb3af5f88b0c
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 14:31:15 +0330

    Arrow functions

--- index.js
+++ index.js
@@ -4,28 +4,11 @@ const courses = [
   { id: 3, name: "c" },
 ];
 
-console.log(courses.includes({ id: 1, name: "a" })); // false
-
 let course = courses.find(function (course) {
   return course.name === "a";
 });
 
+// Arrow function
+course = courses.find((course) => course.name === "a");
+
 console.log(course); // {id: 1, name: 'a'}
-
-course = courses.find(function (course) {
-  return course.name === "f";
-});
-
-console.log(course); // undefined
-
-let courseIndex = courses.findIndex(function (course) {
-  return course.name === "a";
-});
-
-console.log(courseIndex); // 0
-
-courseIndex = courses.findIndex(function (course) {
-  return course.name === "f";
-});
-
-console.log(courseIndex); // -1
