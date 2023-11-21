52c55f89d01a059ba914a286aca990ffa89680b3
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 19:01:32 +0330

    Macing an array

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const numbers = [1, -1, 2, 3];
 
-const filtered = numbers.filter((n) => n >= 0);
+const items = numbers.map((n) => `<li>${n}</li>`);
 
-console.log(filtered);
+const html = "<ul>\n" + items.join("\n") + "\n</ul>\n";
+
+console.log(html);
+//------------------
+const objs1 = numbers.map((n) => {
+  return { value: n };
+});
+
+const objs2 = numbers.map((n) => {
+  value: n;
+}); // {} consider of code's block not object literal
+// to fix that we can use this:
+const objs = numbers.map((n) => ({ value: n }));
+
+console.log(objs);
+// -----------------
+const result = numbers
+  .filter((n) => n >= 0)
+  .map((n) => ({ value: n }))
+  .filter((obj) => obj.value > 1)
+  .map((obj) => obj.value);
+
+console.log(result);
