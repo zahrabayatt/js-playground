088048032ce731fecaf15d8c1960b5e7685e4578
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 12:36:46 +0330

    Exercise - blog post object

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-const address1 = new Address("a", "b", "c");
-const address2 = new Address("a", "b", "c");
-address2.color = "red";
+const post = {
+  title: "a",
+  body: "b",
+  author: "c",
+  views: 12,
+  comments: [
+    { author: "d", body: "f" },
+    { author: "d", body: "f" },
+  ],
+  isLive: false,
+};
 
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-
-// Constructor Function
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-
-function areEqual(address1, address2) {
-  if (getNumberOfProperty(address1) !== getNumberOfProperty(address2))
-    return false;
-  for (const key in address1) {
-    if (!(key in address2) || address1[key] !== address2[key]) return false;
-  }
-
-  return true;
-}
-
-function getNumberOfProperty(obj) {
-  return Object.keys(obj).length;
-}
-
-function areSame(address1, address2) {
-  return address1 === address2;
-}
+console.log(post);
