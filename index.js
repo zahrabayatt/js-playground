b45684c91c72cc56c84a0e41de02f767a8a8e705
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 11:59:03 +0330

    Date

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-// Escape notation:
-let message = "This is my \n first message ";
+// more info about date format supported by Date:
+// https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
 
-const name = "John";
-message = "Hi " + name + ",\n";
+const now = new Date();
+const data1 = new Date("May 11 2023 09:00 ");
+// month is zero base : 0 represent january and 11 represent december
+// const date2 = new Date(2023, 4, 11, 9, 0); by default each arg is 0 so we can ignore minute in date2
+const date2 = new Date(2023, 4, 11, 9);
 
-// One problem with escape notation is that we can't visualize text in the code as it appears. In this situation, we can use a template literal.
+// all date objects have bunch of get and set methods:
+now.setFullYear(2018);
 
-// Template literal ``
-let another = `This is my
-first message`;
-// We can use a template literal for an email message
-another = `Hi John,
-
-Thank you for joining my mailing list.
-
-Regards,
-Zahra Bayat
-`;
-
-another = `Hi ${name} ${2 + 3},
-
-Thank you for shopping from us.
-`;
-
-console.log(message);
-console.log(another);
+console.log(now.toDateString()); // Wed Nov 21 2018
+console.log(now.toTimeString()); // 11:58:16 GMT+0330 (Iran Standard Time)
+console.log(now.toISOString()); // 2018-11-21T08:28:16.233Z - common use in web application for transforming date between client and server
