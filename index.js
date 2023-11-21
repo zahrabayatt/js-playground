7bff4873d06f8c026d668c0955f4e3a68d87e670
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 23:30:45 +0330

    Local vs. global scopes

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-const p = {
-  firstName: "Zahra",
-  lastName: "Bayat",
-};
-
-console.log(`${p.firstName} ${p.lastName}`);
-
-// we may use full name in a lot of code so it is better to move it in object as method
-
-const person = {
-  firstName: "Zahra",
-  lastName: "Bayat",
-  fullName() {
-    // short syntax of fullName: function() {}
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
-
-console.log(person.fullName());
-
-// we can't use fullName like this:
-// person.fullName;
-// person.fullName = 'John Smith';
-
-// in this situation we can use getter and setter
-
-const another = {
-  firstName: "Zahra",
-  lastName: "Bayat",
-  set fullName(value) {
-    if (typeof value !== "string") throw new Error("Value is not a string.");
-
-    const parts = value.split(" ");
-    if (parts.length !== 2) throw new Error("Enter a first and last name.");
-
-    this.firstName = parts[0];
-    this.lastName = parts[1];
-  },
-};
-
-try {
-  another.fullName = "";
-} catch (error) {
-  alert(error.message);
+{
+  // local scope
+  const message = "hi";
 }
 
-console.log(another.fullName);
+const me = "Global";
+
+function start() {
+  console.log(me); // avoid this and this is bad practice don't share global variables
+  // local scope
+  let message = "hello";
+  const color = "blue";
+
+  if (true) {
+    const color = "yellow";
+    const another = "bye";
+    message = "hi";
+    console.log(color, another, message); // yellow bye hi
+  }
+
+  //console.log(another); // error
+  console.log(message, color); // hi blue
+}
+
+start();
+
+// global scope
+
+// const color = "red";
+// const color = "green";
+
+//console.log(message); // error
