c312b7db0b7c0caed447fb5ca3316c16ea06b717
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 12:07:18 +0330

    Exercise - Address object

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// more info about date format supported by Date:
-// https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
+const address = {
+  street: "St.1",
+  city: "Tehran",
+  zipCode: 111111,
+};
+showAddress(address);
 
-const now = new Date();
-const data1 = new Date("May 11 2023 09:00 ");
-// month is zero base : 0 represent january and 11 represent december
-// const date2 = new Date(2023, 4, 11, 9, 0); by default each arg is 0 so we can ignore minute in date2
-const date2 = new Date(2023, 4, 11, 9);
-
-// all date objects have bunch of get and set methods:
-now.setFullYear(2018);
-
-console.log(now.toDateString()); // Wed Nov 21 2018
-console.log(now.toTimeString()); // 11:58:16 GMT+0330 (Iran Standard Time)
-console.log(now.toISOString()); // 2018-11-21T08:28:16.233Z - common use in web application for transforming date between client and server
+function showAddress(address) {
+  for (const key in address) {
+    console.log(key, address[key]);
+  }
+}
