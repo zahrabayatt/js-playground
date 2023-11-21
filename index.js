de65bfa7b58f2d3cc68ef87db5cd845475249f4e
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 20:19:04 +0330

    Exercise - Movies

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const numbers = [1, 2, 3, 4, 6];
+const movies = [
+  { title: "a", year: 2023, rating: 4.5 },
+  { title: "b", year: 2023, rating: 4.7 },
+  { title: "c", year: 2023, rating: 3 },
+  { title: "d", year: 2014, rating: 4.5 },
+];
 
-const max = getMax(numbers);
+const output = movies
+  .filter((m) => m.year === 2023 && m.rating >= 4)
+  //   .sort((a, b) => {
+  //     // descending sort
+  //     if (a.rating < b.rating) return 1;
+  //     if (a.rating > b.rating) return -1;
+  //     return 0;
+  //   })
+  .sort((a, b) => b.rating - a.rating) // descending sort - both rating are number so we can use short way
+  .map((obj) => obj.title);
 
-console.log(max);
-
-function getMax(array) {
-  if (array.length === 0) return undefined;
-
-  return array.reduce((a, b) => (a > b ? a : b));
-}
+console.log(output);
