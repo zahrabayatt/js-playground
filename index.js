de32221f8d75c73dfb89766706b1255c37c5e179
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 20:01:19 +0330

    Exercise - Count Occurrences

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 1];
 
-const output = move(numbers, 1, -1);
+const count = countOccurrences(numbers, 10);
 
-console.log(output);
+console.log(count);
 
-function move(array, index, offset) {
-  const position = index + offset;
-  if (array.length <= position) {
-    console.error("Invalid offset");
-    return;
-  }
-
-  const output = [...array];
-  const element = output.splice(index, 1)[0];
-  output.splice(position, 0, element);
-  return output;
+function countOccurrences(array, searchElement) {
+  return array.reduce(
+    (accumulator, current) =>
+      current === searchElement ? accumulator + 1 : accumulator,
+    0
+  );
 }
