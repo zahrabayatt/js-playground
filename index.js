abbd2355136f671a66f75d3a31ef9317a2dc1d97
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 19:45:26 +0330

    Exercise - Moving an element

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const numbers = [1, 2, 3, 4];
 
-const output = except(numbers, [1]);
+const output = move(numbers, 1, -1);
 
 console.log(output);
 
-function except(array, excluded) {
-  const output = [];
-  for (const element of array) {
-    if (!excluded.includes(element)) output.push(element);
+function move(array, index, offset) {
+  const position = index + offset;
+  if (array.length <= position) {
+    console.error("Invalid offset");
+    return;
   }
 
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
   return output;
 }
