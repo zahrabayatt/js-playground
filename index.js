9cc89caae3fec4c7e94e26f21a4b236addb35734
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 21:42:23 +0330

    Hoisting

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 // In JS, we have two way to defining functions:
-// 1- Function Decurion
+// 1- Function Declaration
 // 2- Function Expression
 
+// we can call function declaration before defining that but we can't do this with expression functions, the reason for this is because JS engine when execute this code, it moves all the functions declaration to top of the file and it called hoisting.
+
+// In summery, hoisting is a process of moving function declaring to top of the file and it done automatically by JS engine.
+
 // Function Declaration
+walk();
 function walk() {
   console.log("walk");
 }
 
-walk();
-
 // Function Expression
-let run = function run() {
+run(); // get error
+const run = function run() {
   console.log("run");
 };
-
-// Anonymous Function Expression
-run = function () {
-  console.log("run faster");
-};
-
-let move = run;
-
-run();
-move();
