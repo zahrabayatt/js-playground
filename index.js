2611a925abcaf447faebacc0e284cab45f926df1
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 21:53:26 +0330

    Arguments

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// In JS, we have two way to defining functions:
-// 1- Function Declaration
-// 2- Function Expression
-
-// we can call function declaration before defining that but we can't do this with expression functions, the reason for this is because JS engine when execute this code, it moves all the functions declaration to top of the file and it called hoisting.
-
-// In summery, hoisting is a process of moving function declaring to top of the file and it done automatically by JS engine.
-
-// Function Declaration
-walk();
-function walk() {
-  console.log("walk");
+// arguments of function are dynamic.
+function sum1(a, b) {
+  console.log(arguments);
+  return a + b;
 }
 
-// Function Expression
-run(); // get error
-const run = function run() {
-  console.log("run");
-};
+console.log(sum1(1, 2));
+console.log(sum1(1)); // NaN - 1 + undefined
+
+function sum() {
+  let total = 0;
+  // plain objects are not iterable so we can not use them with for-of but arguments object has exception and because it has symbol.iterate, it is iterable and we can use it with for-of
+  for (const value of arguments) {
+    total += value;
+  }
+
+  return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
