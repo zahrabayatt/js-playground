593e1f55f9c67fbb5af5da331d0e2838df326a4e
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 12:13:07 +0330

    Exercise - Factory and constructor functions

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const address = {
-  street: "St.1",
-  city: "Tehran",
-  zipCode: 111111,
-};
-showAddress(address);
+// const address = createAddress("a", "b", "c");
+const address = new Address("a", "b", "c");
+console.log(address);
 
-function showAddress(address) {
-  for (const key in address) {
-    console.log(key, address[key]);
-  }
+// Factory Function
+function createAddress(street, city, zipCode) {
+  return {
+    street,
+    city,
+    zipCode,
+  };
+}
+
+// Constructor Function
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
 }
