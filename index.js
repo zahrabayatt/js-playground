66dbe80b1ed7a34c274922a2db4f9dc2b697fc4d
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-21 22:00:11 +0330

    Rest operator

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// arguments of function are dynamic.
-function sum1(a, b) {
-  console.log(arguments);
-  return a + b;
+function sum(...args) {
+  console.log(args); // [1,2,3,4,5] - it is array of arguments that pass to function
+
+  return args.reduce((a, b) => a + b);
 }
 
-console.log(sum1(1, 2));
-console.log(sum1(1)); // NaN - 1 + undefined
+console.log(sum(1, 2, 3, 4, 5)); // 15
 
-function sum() {
-  let total = 0;
-  // plain objects are not iterable so we can not use them with for-of but arguments object has exception and because it has symbol.iterate, it is iterable and we can use it with for-of
-  for (const value of arguments) {
-    total += value;
-  }
-
-  return total;
+// another example
+function calculateFinalPrice(discount, ...prices) {
+  const total = prices.reduce((a, b) => a + b);
+  return total * (1 - discount);
 }
 
-console.log(sum(1, 2, 3, 4, 5));
+console.log(calculateFinalPrice(0.1, 20, 30)); // 45
+
+// Rest parameter must be final parameter
