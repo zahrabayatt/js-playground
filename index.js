61b051ca8740fe861521b63fb0d41fc7414a89ed
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-22 23:33:54 +0330

    Exercise - Sum of arguments

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-const video = {
-  title: "a",
-  tags: ["a", "b", "c"],
-  showTags() {
-    // Using 'self' to store a reference to 'this' is a common practice, but it can be improved with modern JavaScript features.
+console.log(sum(1, 2, 3, 4));
+console.log(sum([5, 4, 3]));
 
-    // Approach 1: Using 'self'
-    const self = this;
-    this.tags.forEach(function (tag) {
-      console.log(self.title, tag); // 'self' refers to 'this' in showTags, which points to the 'video' object. this is a bad practice.
-    });
+function sum(...numbers) {
+  if (numbers.length === 1 && Array.isArray(numbers[0])) {
+    numbers = [...numbers[0]];
+  }
 
-    // Approach 2: Using 'bind'
-    this.tags.forEach(
-      function (tag) {
-        console.log(this, tag); // 'this' refers to video object
-      }.bind(this)
-    );
-
-    // Approach 3: Using arrow function
-    this.tags.forEach((tag) => console.log(this, tag));
-    // Arrow functions inherit 'this' from the enclosing function, solving the context issue. It is a recommended modern approach.
-  },
-};
-
-video.showTags();
-
-// Functions are objects, which means we can add built-in properties using dot notation, just like any other object.
-
-function playVideo(a, b) {
-  console.log(this, a, b);
+  return numbers.reduce((a, b) => a + b);
 }
-
-playVideo.call({ name: "Mosh" }, 1, 2); // {name: 'Mosh'} 1 2 - 'call' sets 'this' to the provided object and passes additional arguments individually and call the function.
-
-playVideo.apply({ name: "Zahra" }, [1, 2]); // {name: 'Zahra' } 1 2 - 'apply' sets 'this' and passes additional arguments as an array and call the function.
-
-// playVideo.bind({ name: "Sara" })(); // {name: 'Sara'}
-// OR
-const fn = playVideo.bind({ name: "Sara" }); // 'bind' returns a new function with 'this' set to the provided object and don't call the function.
-fn(); // {name: 'Sara} undefined undefined
-
-playVideo(); // 'this' refers to the global object (e.g., 'window' in a browser)
