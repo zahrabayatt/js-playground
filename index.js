d455df620c2da2c17b46f973f799d4a9bc01fb53
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-22 19:23:25 +0330

    Let vs. var

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-{
-  // local scope
-  const message = "hi";
-}
-
-const me = "Global";
-
 function start() {
-  console.log(me); // avoid this and this is bad practice don't share global variables
-  // local scope
-  let message = "hello";
-  const color = "blue";
-
-  if (true) {
-    const color = "yellow";
-    const another = "bye";
-    message = "hi";
-    console.log(color, another, message); // yellow bye hi
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
   }
 
-  //console.log(another); // error
-  console.log(message, color); // hi blue
+  //console.log(i); // error - Creating a block-scoped variable with 'let' makes it limited to the block instance.
 }
 
 start();
 
-// global scope
+function start1() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
 
-// const color = "red";
-// const color = "green";
+  console.log(i); // 5 - Creating a function-scoped variable with 'var' makes it limited to the function instance.
+}
 
-//console.log(message); // error
+start1();
+
+// var => function-scoped
+// ES6(ES2015): let, const => block-scoped
+
+// When we define a global variable using 'var', it becomes attached to the window object in browsers. This is considered bad practice, as it can lead to issues if a third-party library also uses a variable with the same name. The variable could be overwritten, causing problems in our program.
+
+var color = "red";
+console.log(window.color); // red
+
+let age = 30;
+console.log(window.age); // undefined
+
+// Technically, when we define a function like 'sayHi' without any specific scoping, it becomes attached to the window object. This is also considered bad practice. With modules, we can encapsulate functions, avoiding the pollution of the global scope.
+
+function sayHi() {
+  console.log("hi");
+}
+
+window.sayHi();
