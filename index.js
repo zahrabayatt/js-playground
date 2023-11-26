ca6d6d451b4ec38d83f522b0ea6853c0fd5c6da7
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-26 13:44:46 +0330

    Abstraction, private property and methods

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// Abstraction: Hide the details, Show the essentials
+
+// Closure in JavaScript determines what variables will be accessible to an inner function.
 function Circle(radius) {
+  // let color = 'red'; // it is only a local variable inside the function.
+
+  let defaultLocation = {
+    x: 0,
+    y: 0,
+  };
+  let computeOptimumLocation = function () {
+    //...
+  };
+
   this.radius = radius;
   this.draw = function () {
-    console.log("draw");
+    computeOptimumLocation(0.1); // access to private method (local function)
+    console.log(defaultLocation); // access to private property (local variable)
+    console.log(this.radius); // access to property in method
   };
 }
 
 const circle = new Circle(10);
 
-// iterate in methods and properties of object with for-in
-for (const key in circle) {
-  if (typeof circle[key] !== "function")
-    // only log properties
-    console.log(key, circle[key]);
-}
-
-const keys = Object.keys(circle);
-console.log(keys);
-
-if ("radius" in circle) console.log("Circle has a radius.");
+// can't access to local variable of object
+console.log(circle.computeOptimumLocation); // undefined
