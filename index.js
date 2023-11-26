6524427bd8fb024b8e36b63be708c856a3291138
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-26 13:25:29 +0330

    Adding/Removing property

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-// Object-Oriented Programming (OOP) in JavaScript is a programming paradigm that organizes code into objects, which are instances of classes. Classes define the properties (attributes) and behaviors (methods) that objects can have. This approach allows for the creation of reusable and structured code, making it easier to manage and scale complex programs.
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
 
-// Procedural Programming: Before object-oriented programming, we had procedural programming, that divided a program into a set of functions. So, you have data stored in a bunch of variables and functions that operate on the data. This style of programming is very simple and straight forward, often it's what you will learn as part of your first programming subject at a university. But as your programs grow, you will end up with a bunch of functions that are all over the place.You might find yourself copying and pasting lines of code over and over to make a change to one function and then several other functions break. That's what we call spaghetti code, there is so much interdependency in all these functions it becomes problematic. Object-oriented programming came to solve this problem.
+const circle = new Circle(10);
 
-// The four pillars of Object-Oriented Programming (OOP) are:
+// Adding property
+circle.location = { x: 1 };
+circle["location"] = { x: 1 };
 
-// Encapsulation:
-// Definition: Encapsulation is the bundling of data (variables) and the functions that operate on the data into a single unit, known as a object.
-// Purpose: It helps in hiding the internal implementation details of an object and exposing only what is necessary. This promotes data security and code organization.
+// Example of bracket notation for adding property
+const propertyName = "location";
+circle[propertyName] = { x: 1 };
 
-// Abstraction:
-// Definition: Abstraction involves simplifying complex systems by modeling objects based on the essential features they share, while ignoring unnecessary details.
-// Purpose: It provides a way to focus on the relevant aspects of an object and ignore the irrelevant ones. Abstraction helps in managing complexity and understanding the system at a higher level and also reduce the impact of change.
+const pName = "center-location"; // in dot notation we can use hyphen or septical characters
+circle[pName] = { x: 1 };
 
-// Inheritance:
-// Definition: Inheritance is a mechanism that allows a class (subclass/derived class) to inherit properties and behaviors from another class (superclass/base class). it allows us to eliminate redundant code.
-// Purpose: It promotes code reusability, as common attributes and methods can be defined in a base class and shared by multiple derived classes.
-
-// Polymorphism:
-// Definition: Polymorphism (many forms) allows objects of different types to be treated as objects of a common type. It can take different forms, such as method overloading and method overriding.
-// Purpose: Polymorphism enables flexibility and extensibility in code. It allows a single interface to represent various types, making it easier to write code that can work with objects of different classes.
-
-// These four pillars collectively provide a foundation for designing and organizing code in an object-oriented manner, leading to more modular, scalable, and maintainable software.
-
-// Benefits of OPP:
-
-// 1 - Encapsulation: Using encapsulation, we group related variables and functions together and it makes:
-//  - Reduce complexity + increase reusability
-
-// 2- Abstraction: Using abstraction, we hide the details and the complexity and show only the essentials and it makes:
-// - Reduce complicity + isolate impact of changes
-
-// 3- Inheritance: it makes:
-// - Eliminate redundant code
-
-// 4- Polymorphism: it makes:
-// - Refactor ugly switch/case statements
+// Removing property
+delete circle.location;
+delete circle["center-location"];
