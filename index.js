616bf7b168b9ff8a1cd96461073eb674a7ad3399
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-26 14:23:54 +0330

    Inheritance

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-function Stopwatch() {
-  let startTime,
-    endTime,
-    running,
-    duration = 0;
+// Inheritance is one of the core concepts of object oriented programming that enables an object to take on the properties and methods of another object.
+// And this makes it easy to reuse code in different parts of an application.
 
-  this.start = function () {
-    if (running) {
-      throw new Error("Stopwatch is not started.");
-    }
+// Here's a real example in the last section we define the circle object with this matter.
+// Compute optimum location.
+// Now for this discussion let's imagine you were working with a class.
+// So let's imagine a circle is a class and not an object.
+// I want to explain inheritance in classical terms.
+// So we have the circle class with one method to compute optimum location.
+// Let's imagine tomorrow we're going to add another class to our application called Square and square
+// also needs this method to compute optimal location for knowledge.
+// Imagine the implementation of this method is exactly the same across these two different classes.
+// You don't want to repeat this implementation because if there is a bug in this implementation we have
 
-    running = true;
+// to fix it in multiple places or if we decide to change the implementation and make it better again have
+// to repeat this in different places so that's when inheritance comes to rescue.
+// We can define a new class called shape with this method there and half circle and square inherit this
+// matter from the Shape class.
+// Now inheritance we have a few terms that you should be familiar with because I'm going to use this term
+// throughout this course and this diagram we refer to the Shape class as the base class or superclass
+// or parent class.
+// All these terms are exactly the same.
+// Different tutorials and different books use different terms.
+// In contrast we refer to the circle as the derived class or subclass or child class.
+// And finally we refer to the inheritance relationship as is a relationship.
 
-    startTime = new Date();
-  };
+// So we say circle is a shape that this is the classical definition of inheritance.
+// But as you know in javascript we don't have classes.
+// We only have objects so that's when prototypical inheritance comes in the picture.
 
-  this.stop = function () {
-    if (!running) {
-      throw new Error("Stopwatch has already started.");
-    }
-
-    running = false;
-
-    endTime = new Date();
-
-    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds;
-  };
-  this.reset = function () {
-    startTime = null;
-    endTime = null;
-    running = false;
-    duration = 0;
-  };
-
-  Object.defineProperty(this, "duration", {
-    get: function () {
-      return duration;
-    },
-  });
-}
+// So essentially we have two types of inheritance.
+// Classical and prototypical.
+// And that's one of the topics for interview questions.
+// So what is a prototypical inheritance.
+// That's a topic for the next lecture.
