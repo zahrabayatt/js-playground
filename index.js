3434d19d9e75b7165cb4c004885b34f1b0278727
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-27 20:11:06 +0330

    Iterating instance and prototype members

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ function Circle(radius) {
   // Instance Member
   this.radius = radius;
 
-  // this.draw = function () {
-  //   console.log("draw");
-  // };
-
-  // access to prototype member in instance member
   this.move = function () {
-    this.draw(); // JS engine will find the draw method in prototype
     console.log("move");
   };
 }
 
-// if we define methods in function constructor, for each instance object in memory we going to have copy of the draw method and it going to waste a lot o memories. for solution we can define our method in prototype so we are going to have a single instance of this prototype in the memory, and with prototypical inheritance we can access to draw function.
+const circle = new Circle(12);
 
 // Prototype Member
 Circle.prototype.draw = function () {
   console.log("draw");
 };
 
-// we can overwrite prototype member like this:
-Circle.prototype.toString = function () {
-  return "Circle with radius " + this.radius; // access instance member in prototype member
-}; // even though we have a implementation in prototype object we use this implementation because it is more accessible
+cl.draw();
 
-const circle = new Circle(12);
-circle.move();
-console.log(circle.toString());
+// the order of modify prototype member and creating object doesn't matter, because at the end, prototype member will still be available in circle object because we are dealing with object references so we have single object in memory and as soon as we modify that all the changes are immediately visible.
 
-// we shouldn't call move in draw and call draw in move in the same time because it makes to have a circular dependency
+// iterate over instance members
+// Object.keys only returns instance members
+console.log(Object.keys(circle));
+
+// iterate over all member(instance + prototype)
+for (const key in circle) {
+  console.log(key);
+}
+
+// some docs use 'own' word instead of 'instance'
+
+// check for existing instance member:
+console.log(circle.hasOwnProperty("radius")); // true
+console.log(circle.hasOwnProperty("draw")); // false - because draw is prototype member
