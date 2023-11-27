1a1c72f4fb79d4e93029a64dda1ec43a44a6cbbd
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-27 17:01:35 +0330

    Multi-level  inheritance

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// In JS we don't have classes we have objects, so how we can implement inheritance only using objects?
+let myArray = [];
+console.log(myArray);
 
-// we define a object and put all common methods in this object and somehow we can link other objects to this object and we refer to this object as the prototype of the other object that linked to it.
+// prototype of myArray is array base and the prototype of array base is object base.
 
-// In conclusion, prototype is a objet which is essentially the parent of another object.
+// so myArray drives from array base and array base drives form the object base, this is called multi level inheritance.
 
-//So every object except the 'Object' base has a prototype or parent and it inherits all the members defined in its prototype because in JS, every objects directly or indirectly inherits from 'Object' base. the 'Object' base is the root of all objects in JS and it doesn't have a prototype or parent.
+function Circle(radius) {
+  this.radius = radius;
 
-let x = {};
-console.log(x);
-// every object has a property called __proto__ which is deprecated and faded in console's browsers and we shouldn't directly access it and it exist to help us to trouble shout in console's browsers.
+  this.draw = function () {
+    console.log("draw");
+  };
+}
 
-//__proto__ property is the object which is the prototype of object and it has properties and methods of the object's prototype
+const circle = new Circle(10);
 
-let y = {};
-// x and y has a same instance of prototype which is "Object" base
-console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
+// we create circle object using constructor function.
 
-console.log(x.toString());
-// when we access a property or a method on an object JS engine first looks for that property or method on the object itself. if it can not find it then it looks at prototype for that object. agin if it can not find that member it will look at the prototype of that object all the way up to the root object which we called 'object' base.
-//in conclusion we called this prototypical inheritance in action when accessing a property or a method on an object, JS engin walks up the prototype chain to find the target member.
+// prototype of circle  object is the prototype of all objects that we create using our circle constructor, we call this prototype circle base. the prototype of circle base is object base.
 
-// Remember, A prototype is just a regular object which every object has a prototype or a parent except the root object.
+// every time we call circle constructor to create a new circle object this constructor will create a new object and set its prototype to circle base.
+
+// In conclusion, Object created by a given constructor will have the same prototype.
