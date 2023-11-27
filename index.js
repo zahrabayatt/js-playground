5deafd6ade5f073934cd95cfad2bed55d496973b
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-27 20:17:19 +0330

    Prototype cheat sheet

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-// we should not modify the built in object in JS because it is possible that if we use a library and in that library someone has also extend that to a prototype and added shuffle method but with a different implementation then we got a lot of issue in our code
+// Every object (except the root object) has a prototype (parent).
+// To get the prototype of an object:
+Object.getPrototypeOf(obj);
 
-// In Summery, Don't modify objects you don't own!!
+// In Chrome, you can inspect "__proto__" property. But you should
+// not use that in the code.
 
-Array.prototype.shuffle = function () {
-  //...
-};
+// To get the attributes of a property:
+Object.getOwnPropertyDescriptor(obj, "propertyName");
 
-const array = [];
-array.shuffle();
+// To set the attributes for a property:
+Object.defineProperty(obj, "propertyName", {
+  configurable: false, // cannot be deleted
+  writable: false,
+  enumerable: false,
+});
+
+// Constructors have a "prototype" property. It returns the object
+// that will be used as the prototype for objects created by the constructor.
+Object.prototype === Object.getPrototypeOf({});
+Array.prototype === Object.getPrototypeOf([]);
+
+// All objects created with the same constructor will have the same prototype.
+// A single instance of this prototype will be stored in the memory.
+const x = {};
+const y = {};
+Object.getPrototypeOf(x) === Object.getPrototypeOf(y); // returns true
+
+// Any changes to the prototype will be immediately visible to all objects
+// referencing this prototype.
+
+// When dealing with large number of objects, it's better to put their
+// methods on their prototype. This way, a single instance of the methods
+// will be in the memory.
+Circle.prototype.draw = function () {};
+
+// To get the own/instance properties:
+Object.keys(obj);
+
+// To get all the properties (own + prototype):
+for (let key in obj) {
+}
