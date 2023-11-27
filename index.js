d34392af8064912556c4ecc0917c4b891eef9686
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-27 19:59:53 +0330

    Prototype vs instance members

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 function Circle(radius) {
+  // Instance Member
   this.radius = radius;
-  this.draw = function () {
-    console.log("draw");
+
+  // this.draw = function () {
+  //   console.log("draw");
+  // };
+
+  // access to prototype member in instance member
+  this.move = function () {
+    this.draw(); // JS engine will find the draw method in prototype
+    console.log("move");
   };
 }
 
-const circle = new Circle();
+// if we define methods in function constructor, for each instance object in memory we going to have copy of the draw method and it going to waste a lot o memories. for solution we can define our method in prototype so we are going to have a single instance of this prototype in the memory, and with prototypical inheritance we can access to draw function.
 
-// constructor functions are object so the prototype of Circle constructor is object base.
-// Circle is also the constructor of circle object, so circle.constructor = Circle function.
-console.log(Circle.prototype);
+// Prototype Member
+Circle.prototype.draw = function () {
+  console.log("draw");
+};
 
-// the prototype of the circle is object base.
-console.log(Object.getPrototypeOf(circle));
+// we can overwrite prototype member like this:
+Circle.prototype.toString = function () {
+  return "Circle with radius " + this.radius; // access instance member in prototype member
+}; // even though we have a implementation in prototype object we use this implementation because it is more accessible
 
-// the prototype of both Circle constructor and circle object is same and it is object base.
-console.log(Object.getPrototypeOf(circle) === Circle.prototype); //
+const circle = new Circle(12);
+circle.move();
+console.log(circle.toString());
 
-// another example
-let x = {};
-// when we use object literal for creating object under the hood it translate to let x = new Object();
-// which Object is function Constructor
-
-// so prototype of x is object base and prototype of Object constructor is object base
+// we shouldn't call move in draw and call draw in move in the same time because it makes to have a circular dependency
