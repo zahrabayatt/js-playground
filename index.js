31e98e856cee05da60eccd3a1329889e6a1b7ab0
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-27 15:21:43 +0330

    Prototypes and prototypical inheritance

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-// Inheritance is one of the core concepts of object oriented programming that enables an object to take on the properties and methods of another object.
-// And this makes it easy to reuse code in different parts of an application.
+// In JS we don't have classes we have objects, so how we can implement inheritance only using objects?
 
-// Here's a real example in the last section we define the circle object with this matter.
-// Compute optimum location.
-// Now for this discussion let's imagine you were working with a class.
-// So let's imagine a circle is a class and not an object.
-// I want to explain inheritance in classical terms.
-// So we have the circle class with one method to compute optimum location.
-// Let's imagine tomorrow we're going to add another class to our application called Square and square
-// also needs this method to compute optimal location for knowledge.
-// Imagine the implementation of this method is exactly the same across these two different classes.
-// You don't want to repeat this implementation because if there is a bug in this implementation we have
+// we define a object and put all common methods in this object and somehow we can link other objects to this object and we refer to this object as the prototype of the other object that linked to it.
 
-// to fix it in multiple places or if we decide to change the implementation and make it better again have
-// to repeat this in different places so that's when inheritance comes to rescue.
-// We can define a new class called shape with this method there and half circle and square inherit this
-// matter from the Shape class.
-// Now inheritance we have a few terms that you should be familiar with because I'm going to use this term
-// throughout this course and this diagram we refer to the Shape class as the base class or superclass
-// or parent class.
-// All these terms are exactly the same.
-// Different tutorials and different books use different terms.
-// In contrast we refer to the circle as the derived class or subclass or child class.
-// And finally we refer to the inheritance relationship as is a relationship.
+// In conclusion, prototype is a objet which is essentially the parent of another object.
 
-// So we say circle is a shape that this is the classical definition of inheritance.
-// But as you know in javascript we don't have classes.
-// We only have objects so that's when prototypical inheritance comes in the picture.
+//So every object except the 'Object' base has a prototype or parent and it inherits all the members defined in its prototype because in JS, every objects directly or indirectly inherits from 'Object' base. the 'Object' base is the root of all objects in JS and it doesn't have a prototype or parent.
 
-// So essentially we have two types of inheritance.
-// Classical and prototypical.
-// And that's one of the topics for interview questions.
-// So what is a prototypical inheritance.
-// That's a topic for the next lecture.
+let x = {};
+console.log(x);
+// every object has a property called __proto__ which is deprecated and faded in console's browsers and we shouldn't directly access it and it exist to help us to trouble shout in console's browsers.
+
+//__proto__ property is the object which is the prototype of object and it has properties and methods of the object's prototype
+
+let y = {};
+// x and y has a same instance of prototype which is "Object" base
+console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
+
+console.log(x.toString());
+// when we access a property or a method on an object JS engine first looks for that property or method on the object itself. if it can not find it then it looks at prototype for that object. agin if it can not find that member it will look at the prototype of that object all the way up to the root object which we called 'object' base.
+//in conclusion we called this prototypical inheritance in action when accessing a property or a method on an object, JS engin walks up the prototype chain to find the target member.
+
+// Remember, A prototype is just a regular object which every object has a prototype or a parent except the root object.
