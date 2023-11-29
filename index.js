c54606578d0e7ea7441d9d2aba32d5bd752dbb49
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 22:52:18 +0330

    Hoisting

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-// In ES6, we have classes which is syntax sugar over objects
-// with classes we can use inheritance in modern way.
+// Class Declaration
+class Circle {}
 
-class Circle {
-  constructor(radius) {
-    this.radius = radius;
-    this.move = function () {
-      console.log("move");
-    }; // define method in object instance
-  }
+// Class Expression
+const Square = class {}; // class expression like function expression terminate with semicolon
 
-  draw() {
-    console.log("draw");
-  } // define method in object's prototype
-}
-
-const c = new Circle(10); // Classes in ES6 forces developer to use new keywords unless they got the error
-console.log(c);
-
-console.log(typeof c); // function
-// to understand this : head over https://babel.io (JS compiler) to see result of completion of this code to understand why c is function.
+// class expression and declaration and function expression didn't hoist but function declaration hoisted.
