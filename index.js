000200177cd3b6a1263dfce4bbbd0214f355a124
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 15:03:07 +0330

    Resetting the constructor

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ Circle.prototype.draw = function () {
   console.log("draw");
 };
 
-// Circle prototype is like this (Circle base):
-//Circle.prototype = Object.create(object.prototype);
-// Circle inherit form Shape (Circle base that inherit form Shape base)
+// Circle inherit form Shape
 Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+const c = new Circle(1);
+
+// there is a issue with above approach:
+
+// we expect a constructor of circle object refer to circle constructor function and we can create object with new keyword :
+// Circle.prototype.constructor = Circle;
+// new Circle.prototype.constructor(1) = new Circle(1);
+// But it is like this:
+// Circle.prototype.constructor = Shape;
+// new Circle.prototype.constructor(1) = new Shape(1);
+// it happen because we reset prototype and to prevent this issue we need to reset constructor : line 17
