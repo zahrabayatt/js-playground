f66b398b5cdd241b37d89973ccdfecf35dcb86ee
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 14:34:07 +0330

    Creating your own prototypical inheritance

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-// putting all method in prototype is not good and it runs sorts of issue
+function Shape() {}
 
-// Premature optimization is the root of all evils
+Shape.prototype.duplicate = function () {
+  console.log("duplicate");
+};
 
-function Stopwatch() {
-  let startTime,
-    endTime,
-    running,
-    duration = 0;
-
-  object.defineProperty(this, "startTime", {
-    get: function () {
-      return startTime;
-    },
-  });
-
-  object.defineProperty(this, "endTime", {
-    get: function () {
-      return endTime;
-    },
-  });
-
-  object.defineProperty(this, "running", {
-    get: function () {
-      return running;
-    },
-  });
-  Object.defineProperty(this, "duration", {
-    get: function () {
-      return duration;
-    },
-  });
+function Circle(radius) {
+  this.radius = radius;
 }
-Stopwatch.prototype.start = function () {
-  if (this.running) {
-    throw new Error("Stopwatch is not started.");
-  }
 
-  this.running = true;
-
-  this.startTime = new Date();
+Circle.prototype.draw = function () {
+  console.log("draw");
 };
 
-Stopwatch.prototype.stop = function () {
-  if (!this.running) {
-    throw new Error("Stopwatch has already started.");
-  }
-
-  this.running = false;
-
-  this.endTime = new Date();
-
-  const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
-  this.duration += seconds;
-};
-
-Stopwatch.prototype.reset = function () {
-  this.startTime = null;
-  this.endTime = null;
-  this.running = false;
-  this.duration = 0;
-};
+// Circle prototype is like this (Circle base):
+//Circle.prototype = Object.create(object.prototype);
+// Circle inherit form Shape (Circle base that inherit form Shape base)
+Circle.prototype = Object.create(Shape.prototype);
