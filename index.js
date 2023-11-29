71efc7a00426142b29fc12572e4900822e14438f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 22:34:10 +0330

    Exercise- Polymorphism

--- index.js
+++ index.js
@@ -18,7 +18,39 @@ function HtmlSelectElement(items = []) {
   this.removeItem = function (item) {
     this.items.splice(this.items.indexOf(item), 1);
   };
+
+  this.render = function () {
+    return `
+<select>${this.items
+      .map(
+        (item) => `
+  <option>${item}</option>`
+      )
+      .join("")}
+</select>
+`;
+  };
 }
 
-HtmlSelectElement.prototype = new HtmlElement(); // because we have click method in object instance
+HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+function HtmlImageElement(src) {
+  this.src = src;
+
+  this.render = function () {
+    return `<img src="${this.src}" />`;
+  };
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+const elements = [
+  new HtmlSelectElement([1, 2, 3]),
+  new HtmlImageElement("http://"),
+];
+
+for (const element of elements) {
+  console.log(element.render());
+}
