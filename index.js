19c41dfa104d88fbec046a1680d422b3bab09b38
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 15:08:57 +0330

    Calling the super constructor

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function Shape() {}
+function Shape(color) {
+  this.color = color;
+}
 
 Shape.prototype.duplicate = function () {
   console.log("duplicate");
 };
 
-function Circle(radius) {
+function Circle(radius, color) {
+  // call super constructor
+  Shape.call(this, color); // we bind this refer to object created by Circle in Shape parent
+
   this.radius = radius;
 }
 
@@ -16,14 +21,4 @@ Circle.prototype.draw = function () {
 Circle.prototype = Object.create(Shape.prototype);
 Circle.prototype.constructor = Circle;
 
-const c = new Circle(1);
-
-// there is a issue with above approach:
-
-// we expect a constructor of circle object refer to circle constructor function and we can create object with new keyword :
-// Circle.prototype.constructor = Circle;
-// new Circle.prototype.constructor(1) = new Circle(1);
-// But it is like this:
-// Circle.prototype.constructor = Shape;
-// new Circle.prototype.constructor(1) = new Shape(1);
-// it happen because we reset prototype and to prevent this issue we need to reset constructor : line 17
+const c = new Circle(1, "red");
