2a2f6f46395a3cf027c50127fc7a0728b2691915
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 23:59:33 +0330

    The this keyword in ES6

--- index.js
+++ index.js
@@ -1,48 +1,28 @@
-// In classical OPP language we have two types of methods:
-// 1- instance method
-// 2- static method
+"use strict"; // enable strict mode
 
-class Circle {
-  constructor(radius) {
-    this.radius = radius;
-  }
+const Circle = function () {
+  this.draw = function () {
+    console.log(this);
+  };
+};
 
-  // Instance Method
-  draw() {}
+const c = new Circle();
+// Method Call
+c.draw(); // this will point to c object
 
-  // Static Method
-  static parse(str) {}
-}
+const draw = c.draw;
 
-const circle = new Circle(1);
-console.log(circle.draw()); // calling instance method
-console.log(Circle.parse()); // calling static method
+// Function Call
+draw(); // this will point to global object - if strict mode is enable this is undefined
 
-//Example
-
+// In ES6:
 class Square {
-  constructor(size) {
-    this.size = size;
-  }
-
-  // Instance Method
-  draw() {}
-
-  // Static Method
-  static parse(str) {
-    const size = JSON.parse(str).size;
-    return new Square(size);
+  draw() {
+    console.log(this);
   }
 }
 
-const square = Square.parse('{"size": 1 }');
-console.log(square);
-
-// another example: for utility function
-class Math2 {
-  static abs(value) {
-    //...
-  }
-}
-
-Math2.abs();
+const s = new Square();
+s.draw(); // this will point to s object
+const d = s.draw;
+d(); // undefined - where ever strict mode enable or untenable - the reason is because classes executed by default in strict mode
