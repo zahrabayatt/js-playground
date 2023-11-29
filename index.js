240b73834461dd5512c72f98cc559ec946f1272d
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-29 23:49:33 +0330

    Static methods

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-// Class Declaration
-class Circle {}
+// In classical OPP language we have two types of methods:
+// 1- instance method
+// 2- static method
 
-// Class Expression
-const Square = class {}; // class expression like function expression terminate with semicolon
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
 
-// class expression and declaration and function expression didn't hoist but function declaration hoisted.
+  // Instance Method
+  draw() {}
+
+  // Static Method
+  static parse(str) {}
+}
+
+const circle = new Circle(1);
+console.log(circle.draw()); // calling instance method
+console.log(Circle.parse()); // calling static method
+
+//Example
+
+class Square {
+  constructor(size) {
+    this.size = size;
+  }
+
+  // Instance Method
+  draw() {}
+
+  // Static Method
+  static parse(str) {
+    const size = JSON.parse(str).size;
+    return new Square(size);
+  }
+}
+
+const square = Square.parse('{"size": 1 }');
+console.log(square);
+
+// another example: for utility function
+class Math2 {
+  static abs(value) {
+    //...
+  }
+}
+
+Math2.abs();
