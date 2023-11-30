db2b92ac626dd65b904251d31d2ba0195d0553e2
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-30 22:00:21 +0330

    Exercise - Stack

--- index.js
+++ index.js
@@ -1,61 +1,33 @@
-class Circle {
-  constructor(radius) {
-    this.radius = radius;
+const _items = new WeakMap();
+
+class Stack {
+  constructor() {
+    _items.set(this, []);
   }
 
-  // These methods will be added to the prototype.
-  draw() {}
-
-  // This will be available on the Circle class (Circle.parse())
-  static parse(str) {}
-}
-
-// Using symbols to implement private properties and methods
-const _size = Symbol();
-const _draw = Symbol();
-
-class Square {
-  constructor(size) {
-    // "Kind of" private property
-    this[_size] = size;
+  get count() {
+    return _items.get(this).length;
   }
 
-  // "Kind of" private method
-  [_draw]() {}
+  peak() {
+    if (this.count === 0) {
+      throw new Error("Stack is empty.");
+    }
 
-  // By "kind of" I mean: these properties and methods are essentally
-  // part of the object and are accessible from the outside. But accessing
-  // them is hard and awkward.
-}
-
-// using WeakMaps to implement private properties and methods
-const _width = new WeakMap();
-
-class Rectangle {
-  constructor(width) {
-    _width.set(this, width);
+    return _items.get(this)[this.count - 1];
   }
 
-  draw() {
-    console.log("Rectangle with width" + _width.get(this));
+  pop() {
+    if (this.count === 0) {
+      throw new Error("Stack is empty.");
+    }
+
+    return _items.get(this).pop();
+  }
+
+  push(item) {
+    _items.get(this).push(item);
   }
 }
 
-// WeakMaps give us better protection than symbols. There is no way
-// to access private members implemented using WeakMaps from the
-// outside of an object.
-
-// Inheritance
-class Triangle extends Shape {
-  constructor(color) {
-    // To call the base constructor
-    super(color);
-  }
-
-  draw() {
-    // Call the base method
-    super.draw();
-
-    // Do some other stuff here
-  }
-}
+const stack = new Stack();
