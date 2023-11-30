df703651adb871423d0e0dbd8b577ccf2881b353
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-30 14:55:06 +0330

    Getter and setter in ES6 classes

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
-// weakmap which is a new type in ES6 to implement private properties and methods in an object.
-
-// weakmap is essentially a dictionary where keys are objects and values can be anything. the reason we call them weakmap is because the keys are weak. so if there are no references to these keys it will be garbage collected.
-
-// technically we can access this radius private property if we can get access to this weakmap but with modules we can hide this weakmap in a modules and only export the circle class and we don't have access to this weakmap.
-
 const _radius = new WeakMap();
-const _move = new WeakMap();
-
-// we can define weakmap like this but above approach is better
-const privateProps = new WeakMap();
 
 class Circle {
   constructor(radius) {
-    privateProps.set(this, {
-      radius: radius,
-      move: () => {},
-    }); // set
-
-    privateProps.get(this).radius; // access
-
-    //-------------------
-
     _radius.set(this, radius);
-    _move.set(this, function () {
-      console.log("move", this); // this will be point to undefined
-    });
 
-    _move.set(
-      this,
-      () => console.log("move", this) // this inherit from containing function which is construct so it will point to circle object
-    );
+    // solution I : define getter for radius
+    // Object.defineProperty(this, "radius", {
+    //   get: function () {},
+    // });
   }
 
-  draw() {
-    _move.get(this)(); // access to private method inside the class
-    console.log(_radius.get(this)); // access to private property inside the class
+  // solution II : define getter for radius
+  // getRadius() {
+  //   return _radius.get(this);
+  // }
 
-    console.log("draw");
+  // solution III : define getter and setter with new feature in ES6
+  get radius() {
+    return _radius.get(this);
+  }
+
+  set radius(value) {
+    if (value <= 0) throw new Error("invalid radius");
+    _radius.set(this, value);
   }
 }
 
-const c = new Circle(1); // c object doesn't have radius property and we can not access that.
-
-c.draw();
+const c = new Circle(1);
+console.log(c.radius);
+c.radius = 10;
+console.log(c.radius);
