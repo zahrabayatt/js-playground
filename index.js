d62f892a6c230e863e5c63b8556a8d4e7cbd8ebe
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-11-30 14:48:53 +0330

    Private properties using weakmaps

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-// Abstraction: hiding the details and complexity and showing only the essential parts.
-// to implement abstraction we use private properties and methods
+// weakmap which is a new type in ES6 to implement private properties and methods in an object.
 
-// approach I: name private members with starting character underline - this is a bad approach because we can still access to private members
+// weakmap is essentially a dictionary where keys are objects and values can be anything. the reason we call them weakmap is because the keys are weak. so if there are no references to these keys it will be garbage collected.
+
+// technically we can access this radius private property if we can get access to this weakmap but with modules we can hide this weakmap in a modules and only export the circle class and we don't have access to this weakmap.
+
+const _radius = new WeakMap();
+const _move = new WeakMap();
+
+// we can define weakmap like this but above approach is better
+const privateProps = new WeakMap();
+
 class Circle {
   constructor(radius) {
-    this._radius = radius;
+    privateProps.set(this, {
+      radius: radius,
+      move: () => {},
+    }); // set
+
+    privateProps.get(this).radius; // access
+
+    //-------------------
+
+    _radius.set(this, radius);
+    _move.set(this, function () {
+      console.log("move", this); // this will be point to undefined
+    });
+
+    _move.set(
+      this,
+      () => console.log("move", this) // this inherit from containing function which is construct so it will point to circle object
+    );
+  }
+
+  draw() {
+    _move.get(this)(); // access to private method inside the class
+    console.log(_radius.get(this)); // access to private property inside the class
+
+    console.log("draw");
   }
 }
 
-const c = new Circle(1);
-c._radius;
+const c = new Circle(1); // c object doesn't have radius property and we can not access that.
 
-// approach II: using symbols
-// a symbol is essentially a unique identifier and this is not a constructor function so we can not new that up.
-// Symbol return a uniq value - Symbol() doesn't equal to Symbol()
-const _size = Symbol();
-const _draw = Symbol();
-class Square {
-  constructor(radius) {
-    this[_size] = radius;
-  }
-
-  [_draw]() {} // ES6 we have a feature called computed property names so we can add brackets and inside of these brackets we add an expression, when that expression is evaluated the resulting value will be used as the name of a property or method.
-}
-
-const s = new Square(12);
-console.log(Object.getOwnPropertyNames(s)); // []
-
-// we can still access to private property
-const key = Object.getOwnPropertySymbols(s)[0];
-console.log(s[key]); //12
+c.draw();
