6ffffe4801b773edd88640d9252936f09a12591b
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-01 15:07:00 +0330

    Babel

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// ES6 Tooling: these tools are only important if you're building browser application and if you're using JS in Node.js, you don't have to worry about these tools
+//Transpiler : it is the combination of two words (translator + compiler), basically it's a tool that we give with our modern javascript code(ES6) and it will convert our javascript code into code that all browsers can understand(ES5). Babel is an example of a very popular transpiler for modern javascript.
 
-// when we use modern javascript we need two kinds of tools:
+// NPM: it is stand for node package manger and it's a tool that comes with node.js. NPM is a tool that we use to install third party libraries and tools.
 
-// 1) Transpiler : it is the combination of two words (translator + compiler), basically it's a tool that we give with our modern javascript code(ES6) and it will convert our javascript code into code that all browsers can understand(ES5). Babel is an example of a very popular transpiler for modern javascript.
+// npm init --yes : it is a command for initializing a node project, --yes is for add some default value, it creates a file called package.json which is an identification for our application. in this file we have properties like name, version, description and so on.
 
-// 2) Bundler: a module bundler is responsible for combining all our javascript files into a single file which we called Bundle. There are many modules bundlers out there but the most popular one is webpack, so we give all our JS files to webpack and webpack will combine them into a single file and it will minify our code by getting rid of all the whitespace and comments and then it will ugly file our code which basically means it will shorten the name our identifiers like classes, functions, objects and so on. so this will reduce the size of the bundle that will serve the client
+// after initialize a node project we ready to install Babel(https://babeljs.io):
+// we should install three packages:
+// @babel/cli: it is Babel's command line interface which is the tool that is run form the command line like npm. so we run it from the command line and give it the name of our javascript file and then it will convert or compile the javascript code.
+// @babel/core: it is basically the core of babel where all the logic for transpiling code is implemented.
+// @babel/preset-env: in Babel we have a corresponding plugin for every new JS feature starting from ES5 and newer versions so if you want to use only two of these new features, you can install only those two plugins but if you want to have more flexibility you can install this preset and this preset is basically the combination of all these plugins so it understands all the new features in JS from ES6.
+// we install these packages in development dependencies so they're not going to be part of our application and deployed to the production and they are purely on the development machine.
+// npm install --save - dev @babel/core @babel/cli @babel/preset-env
+
+const x = 1;
+// if we want to use Babel to convert above code to code that all browsers can understand, the first thing we need to do is to go to package.json and define a script like this:
+// babel(Babble's CLI) --presets @babel/preset-env (presets we gonna use is env)  index.js --out-dir build
+
+// if we look up in index.js in build folder we see 'use strict' added cause it added by default as a best practice. in this file also const keyword converted to var keyword cause in ES5 we don't have let or const.
