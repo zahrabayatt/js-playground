ae38efb3d72a2f7da81d2f84ca434848cdf29153
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-01 13:56:28 +0330

    ES6 Tooling

--- circle.js
+++ circle.js
@@ -1,16 +1,1 @@
-// by default everything that defined in a module is considered to be private so it won't be accessible to the outside unless we explicitly export it.
 
-// Implementor Detail
-const _radius = new WeakMap();
-
-// Public Interface
-// for exporting in ES6 modules we use export keyword before them:
-export class Circle {
-  constructor(radius) {
-    _radius.set(this, radius);
-  }
-
-  draw() {
-    console.log("Circle with radius " + _radius.get(this));
-  }
-}
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-// for import modules in ES6 modules we use this syntax:
-// in this syntax path is related path to this target module.
-// note that we don't need to add the extension .js in circle.js because thats assumed by default.
-// import { Circle } from "./circle";
+// ES6 Tooling: these tools are only important if you're building browser application and if you're using JS in Node.js, you don't have to worry about these tools
 
-// we got the syntax error which says 'Cannot use import statement outside a module', there is a proper way to fix this issue using a webpack but for now we use a quick workaround that we shouldn't use in production and it is just for demo.
-// workaround: we add type=module to script of index.js in index.html and we also should specify a path of file with extension
-import { Circle } from "./circle.js";
+// when we use modern javascript we need two kinds of tools:
 
-const c = new Circle(10);
-c.draw();
+// 1) Transpiler : it is the combination of two words (translator + compiler), basically it's a tool that we give with our modern javascript code(ES6) and it will convert our javascript code into code that all browsers can understand(ES5). Babel is an example of a very popular transpiler for modern javascript.
+
+// 2) Bundler: a module bundler is responsible for combining all our javascript files into a single file which we called Bundle. There are many modules bundlers out there but the most popular one is webpack, so we give all our JS files to webpack and webpack will combine them into a single file and it will minify our code by getting rid of all the whitespace and comments and then it will ugly file our code which basically means it will shorten the name our identifiers like classes, functions, objects and so on. so this will reduce the size of the bundle that will serve the client
