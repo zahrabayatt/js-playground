17645d883c236389587731f6f03f07a1c2b6d602
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-01 13:12:57 +0330

    ES6 modules

--- circle.js
+++ circle.js
@@ -4,7 +4,8 @@
 const _radius = new WeakMap();
 
 // Public Interface
-class Circle {
+// for exporting in ES6 modules we use export keyword before them:
+export class Circle {
   constructor(radius) {
     _radius.set(this, radius);
   }
@@ -13,26 +14,3 @@ class Circle {
     console.log("Circle with radius " + _radius.get(this));
   }
 }
-
-// the way we export objects in CommonJS format is like this:
-// keyword module refers to the current module
-// this module has a property called exports and it is an object, we can add one or more properties to this object so we can add a property called circle and set that to a circle class here.
-// module.exports.Circle = Circle;
-
-// because in this example we are exporting only a single object we can also do something like this:
-module.exports = Circle;
-
-// if we had multiple classes here and we wanted to export them together we cloud do something like this:
-//  so this module that exports is an object with two properties Square and Shape and then we can import this object inside of our index.js and access these properties.
-// class Shape {
-//   draw() {
-//     console.log("draw");
-//   }
-// }
-// class Square {
-//   draw() {
-//     console.log("draw");
-//   }
-// }
-// module.exports.Square = Square;
-// module.exports.Shape = Shape;
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// CommonJS: it is module format that is used in Node.js.
-// there is a basic rule of thumb about modularity: Things that are highly related should go together. this is what we called Cohesion in software engineering.
-// in this example _radius and Circle class are highly related so we can move them outside of this index.js and put them in a separate file called circle.js.
-
-// in the top the file we import the modules in CommonJS format that we want like this:
-// we use require function to import modules and it is part of CommonJS format.
-// we pass a related path to this target module use to require function.
+// for import modules in ES6 modules we use this syntax:
+// in this syntax path is related path to this target module.
 // note that we don't need to add the extension .js in circle.js because thats assumed by default.
-// require function returned what is returned from given module.
-const Circle = require("./circle");
-// const { Square, Shape } = require("./circle");
+// import { Circle } from "./circle";
+
+// we got the syntax error which says 'Cannot use import statement outside a module', there is a proper way to fix this issue using a webpack but for now we use a quick workaround that we shouldn't use in production and it is just for demo.
+// workaround: we add type=module to script of index.js in index.html and we also should specify a path of file with extension
+import { Circle } from "./circle.js";
 
 const c = new Circle(10);
 c.draw();
