31621076da638b442bdc0c094c9f64cd11554c27
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-12 21:39:24 +0330

    Type casting

---
+++ index.js
@@ -0,0 +1,38 @@
+// Type Casting:
+// Type conversion (or typecasting) means the transfer of data from one data type to another. Implicit conversion happens when the compiler (for compiled languages) or runtime (for script languages like JavaScript) automatically converts data types. The source code can also explicitly require a conversion to take place.
+
+// Visit the following resources to learn more:
+// https://javascript.info/type-conversions
+// https://www.w3schools.com/js/js_type_conversion.asp
+// https://www.tutorialspoint.com/type-casting-in-javascript
+// https://www.geeksforgeeks.org/javascript-type-conversion/
+
+// Implicit Type Casting:
+// Implicit type conversion happens when the compiler or runtime automatically converts data types. JavaScript is loosely typed language and most of the time operators automatically convert a value to the right type.
+
+// Visit the following resources to learn more:
+// https://www.geeksforgeeks.org/javascript-type-conversion/
+// https://www.w3schools.com/js/js_type_conversion.asp
+// https://www.tutorialspoint.com/explain-typecasting-in-javascript
+// https://dev.to/promisetochi/what-you-need-to-know-about-javascripts-implicit-coercion-e23
+
+// Explicit Type Casting:
+// Type casting means transferring data from one data type to another by explicitly specifying the type to convert the given data to. Explicit type casting is normally done to make data compatible with other variables. Examples of typecasting methods are parseInt(), parseFloat(), toString().
+
+// Visit the following resources to learn more:
+// https://www.c-sharpcorner.com/article/type-conversions-in-javascript/
+// https://www.scaler.com/topics/javascript/type-conversion-in-javascript/
+// https://www.youtube.com/watch?v=VQLYiFqetZM
+// https://developer.mozilla.org/en-US/docs/Glossary/Type_Conversion
+// https://www.tutorialspoint.com/explain-typecasting-in-javascript
+
+// Type Conversion/Coercion
+// Type coercion is the automatic or implicit conversion of values from one data type to another (such as strings to numbers). Type conversion is similar to type coercion because they convert values from one data type to another with one key difference — type coercion is implicit. In contrast, type conversion can be either implicit or explicit.
+
+// Visit the following resources to learn more:
+
+// https://developer.mozilla.org/en-US/docs/Glossary/Type_Conversion
+// https://developer.mozilla.org/en-US/docs/Glossary/Type_coercion
+// https://www.youtube.com/watch?v=jfQyMPzPTjY
+// https://www.youtube.com/watch?v=wFiVtqe1osM&list=PL2xC9SMbSc0Zxwssi4E_SN0xFcCKxB4Fx
+// https://www.youtube.com/watch?v=GKJh3vdat8A&list=PL2xC9SMbSc0Zxwssi4E_SN0xFcCKxB4Fx&index=2
