caa9352a3b235b7008b2daa4ac723f45b5ed49de
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-14 19:52:30 +0330

    Labeled Statements

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-// Equality Comparisons:
-// Comparison operators are used in logical statements to determine equality or difference between variables or values. Comparison operators can be used in conditional statements to compare values and take action depending on the result.
+// Labeled Statements:
+// JavaScript label statements are used to prefix a label to an identifier. It can be used with break and continue statement to control the flow more precisely.
 
-// 1- Value Comparison Operator:
-// In javascript, the == operator does the type conversion of the operands before comparison, whereas the === operator compares the values and the data types of the operands. The Object.is() method determines whether two values are the same value: Object.is(value1, value2).
-// Object.is() is not equivalent to the == operator. The == operator applies various coercions to both sides (if they are not the same type) before testing for equality (resulting in such behavior as "" == false being true), but Object.is() doesn’t coerce either value.
-// Object.is() is also not equivalent to the === operator. The only difference between Object.is() and === is in their treatment of signed zeros and NaN values. The === operator (and the == operator) treats the number values -0 and +0 as equal but treats NaN as not equal to each other.
+// A label is simply an identifier followed by a colon(:) that is applied to a block of code.
 
 // Visit the following resources to learn more:
-// https://www.scaler.com/topics/javascript/difference-between-double-equals-and-triple-equals-in-javascript/
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value_equality_using_object.is
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label
+// https://www.tutorialspoint.com/What-are-label-statements-in-JavaScript
+// https://javascript.info/while-for#labels-for-break-continue
 
-// 2- Equality Algorithms:
-// Equality algorithms are used to perform equality comparisons of values or variables in JavaScript. Each equality algorithm works slightly differently, and the one you use depends on the type of comparison you want to make.
+// The first for statement is labeled "loop1"
+loop1: for (let i = 0; i < 3; i++) {
+  // The second for statement is labeled "loop2"
+  loop2: for (let j = 0; j < 3; j++) {
+    if (i === 1 && j === 1) {
+      continue loop1;
+    }
+    console.log(`i = ${i}, j = ${j}`);
+  }
+}
 
-// Visit the following resources to learn more:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
-
-// - isLooselyEqual
-// isLooselyEqual checks whether its two operands are equal, returning a Boolean result. It attempts to convert and compare operands that are of different types.
-
-// Visit the following resources to learn more:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Equality
-// https://javascript.info/comparison
-
-// - isStrictlyEqual
-// isStrictlyEqual checks whether its two operands are equal, returning a Boolean result. It always considers operands of different types to be different.
-
-// Visit the following resources to learn more:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_equality
-// https://javascript.info/comparison
-
-// - Same value zero
-// SameValueZero equality determines whether two values are functionally identical in all contexts with +0 and -0 are also considered equal.
-
-// Visit the following resources to learn more:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value-zero_equality
-
-// - Same value
-// SameValue equality determines whether two values are functionally identical in all contexts.
-
-// Visit the following resources to learn more:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value_equality_using_object.is
+// Logs:
+// i = 0, j = 0
+// i = 0, j = 1
+// i = 0, j = 2
+// i = 1, j = 0
+// i = 2, j = 0
+// i = 2, j = 1
+// i = 2, j = 2
