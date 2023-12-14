817aa7970391bbdfcdcbd92c41797fbeccf31399
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-14 19:48:37 +0330

    Equality Comparisons

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-// Data Structures
-// A Data structure is a format to organize, manage and store data in a way that allows efficient access and modification. JavaScript has primitive (built-in) and non-primitive (not built-in) data structures. Primitive data structures come by default with the programming language and you can implement them out of the box (like arrays and objects). Non-primitive data structures don’t come by default and you have to code them up if you want to use them.
+// Equality Comparisons:
+// Comparison operators are used in logical statements to determine equality or difference between variables or values. Comparison operators can be used in conditional statements to compare values and take action depending on the result.
 
-// index collections - array and type array
-// key collections - map, set, weakmap, weakset
-// structured data - JSON
+// 1- Value Comparison Operator:
+// In javascript, the == operator does the type conversion of the operands before comparison, whereas the === operator compares the values and the data types of the operands. The Object.is() method determines whether two values are the same value: Object.is(value1, value2).
+// Object.is() is not equivalent to the == operator. The == operator applies various coercions to both sides (if they are not the same type) before testing for equality (resulting in such behavior as "" == false being true), but Object.is() doesn’t coerce either value.
+// Object.is() is also not equivalent to the === operator. The only difference between Object.is() and === is in their treatment of signed zeros and NaN values. The === operator (and the == operator) treats the number values -0 and +0 as equal but treats NaN as not equal to each other.
 
-// visit the sections of data structures in following link for more information:
-// https://roadmap.sh/javascript
-// https://youtu.be/nGOnfYNo8F4?si=H1e4BgIeqGcvubjM
-// https://www.youtube.com/watch?v=hLgUTM3FOII&list=PL2xC9SMbSc0Zxwssi4E_SN0xFcCKxB4Fx&index=16
+// Visit the following resources to learn more:
+// https://www.scaler.com/topics/javascript/difference-between-double-equals-and-triple-equals-in-javascript/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value_equality_using_object.is
+
+// 2- Equality Algorithms:
+// Equality algorithms are used to perform equality comparisons of values or variables in JavaScript. Each equality algorithm works slightly differently, and the one you use depends on the type of comparison you want to make.
+
+// Visit the following resources to learn more:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
+
+// - isLooselyEqual
+// isLooselyEqual checks whether its two operands are equal, returning a Boolean result. It attempts to convert and compare operands that are of different types.
+
+// Visit the following resources to learn more:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Equality
+// https://javascript.info/comparison
+
+// - isStrictlyEqual
+// isStrictlyEqual checks whether its two operands are equal, returning a Boolean result. It always considers operands of different types to be different.
+
+// Visit the following resources to learn more:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_equality
+// https://javascript.info/comparison
+
+// - Same value zero
+// SameValueZero equality determines whether two values are functionally identical in all contexts with +0 and -0 are also considered equal.
+
+// Visit the following resources to learn more:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value-zero_equality
+
+// - Same value
+// SameValue equality determines whether two values are functionally identical in all contexts.
+
+// Visit the following resources to learn more:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value_equality_using_object.is
