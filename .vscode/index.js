101aa0f072049ace168f2a29d29f4a35b6648188
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-12-19 22:24:49 +0330

    Asynchronous

--- .vscode/index.js
+++ .vscode/index.js
@@ -58,3 +58,12 @@
 
 // Visit the following resources to learn more:
 // https://www.codeguage.com/courses/advanced-js/promises-introduction
+//--------------------------------------------------------------------------------------------
+//Async/Await
+// async/await is a special syntax to work with promises in a more comfortable fashion. We use async keyword to declare a async function that return a Promise, and the await keyword makes a function wait for a Promise.
+
+// Visit the following resources to learn more:
+// https://javascript.info/async-await
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// https://www.codeguage.com/courses/advanced-js/promises-chaining
+// https://www.codeguage.com/courses/advanced-js/promises-chaining
