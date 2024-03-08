8404b6e06a978963ff9a3d3545e6f0a6bbe228e1
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-03-08 21:03:43 +0330

    Throwing Errors in Async functions

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-// Example - Async & Await
-
-// This feature allows us to chain promises more cleaner and readable.
-
-// old solution
-// fetch("todos/ali.json")
-//   .then((response) => {
-//     if (response.status === 200) return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.log("rejected: ", err);
-//   });
-
-// better solution
+// Example - Throwing Errors in Async functions
 const getTodos = async () => {
-  const response = await fetch("todos/ali.json");
-  //console.log(response);
+  const response = await fetch("todos/alis.json");
+  if (response.status != 200) {
+    throw new Error("cannot fetch the data!");
+  }
   const data = await response.json();
-  //console.log(data);
+
   return data;
 };
-
-const test = getTodos();
-console.log(test); // test is a promise because async functions always return promise.
 
 console.log(1);
 console.log(2);
@@ -32,7 +15,7 @@ console.log(2);
 // non-blocking
 getTodos()
   .then((data) => console.log("resolved: ", data))
-  .catch((err) => console.log("rejected: ", err));
+  .catch((err) => console.log("rejected: ", err.message));
 
 console.log(3);
 console.log(4);
