3c30171189a42726b4c8e2142ae3fd0f2755470d
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-03-08 20:57:36 +0330

    Async & Await

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-// Example - Make HTTPRequest using Fetch API
+// Example - Async & Await
 
-// so far we made HTTPRequest using XMLHTTPRequest object
+// This feature allows us to chain promises more cleaner and readable.
 
-fetch("todos/alis.json")
-  .then((response) => {
-    //console.log("resolved: ", response);
-    if (response.status === 200) return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    // only catches errors, if we got some kind of networking errors
-    console.log("rejected: ", err);
-  });
+// old solution
+// fetch("todos/ali.json")
+//   .then((response) => {
+//     if (response.status === 200) return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected: ", err);
+//   });
+
+// better solution
+const getTodos = async () => {
+  const response = await fetch("todos/ali.json");
+  //console.log(response);
+  const data = await response.json();
+  //console.log(data);
+  return data;
+};
+
+const test = getTodos();
+console.log(test); // test is a promise because async functions always return promise.
+
+console.log(1);
+console.log(2);
+
+// non-blocking
+getTodos()
+  .then((data) => console.log("resolved: ", data))
+  .catch((err) => console.log("rejected: ", err));
+
+console.log(3);
+console.log(4);
