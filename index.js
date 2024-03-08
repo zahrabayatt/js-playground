dd9a4aadf41a6006934b5ecc75bf5a7a41e7d087
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-03-08 19:37:57 +0330

    Using JSON data

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-// Example - callback functions
+// Example - using JSON data
 const getTodos = (callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
       callback("could not fetch the data.", undefined);
     }
   });
 
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  //request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.open("GET", "todos.json"); // read data from json file
+
   request.send();
 };
 
-console.log(1);
-console.log(2);
 getTodos((err, data) => {
   if (err) {
     console.log(err);
@@ -23,6 +24,3 @@ getTodos((err, data) => {
     console.log(data);
   }
 });
-
-console.log(3);
-console.log(4);
