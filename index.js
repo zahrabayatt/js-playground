01f94e4c14f0fab03ff15ad67a4812e16ce174bf
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-03-08 19:21:45 +0330

    Callback functions

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-// Example - HTTP Request and status codes
-const request = new XMLHttpRequest();
+// Example - callback functions
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.readyState === 4) {
-    console.log("could not fetch the data.");
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("could not fetch the data.", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.send();
+};
+
+console.log(1);
+console.log(2);
+getTodos((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
 
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-request.send();
-
-// Make HTTP requests to get data from another server
-// We make these requests to API endpoints
-// Example API endpoint: http://www.musicapi.com/artist/moby
-
-// // what is readyState? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
-
-// // what is status code? https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+console.log(3);
+console.log(4);
