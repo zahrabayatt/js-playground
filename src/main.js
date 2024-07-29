a008d18e8847ff810e6d6bbd9bbd48c0bfc7b4c2
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-07-30 00:23:20 +0330

    Selecting elements in the DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
+// getElementById()
 
+const title = document.getElementById("main-heading");
+
+console.log(title);
+
+// getElementByClassName()
+
+const listItems = document.getElementsByClassName("list-items");
+
+console.log(listItems);
+
+// getElementByTagName()
+
+const liItems = document.getElementsByTagName("li");
+
+console.log(liItems);
+
+// querySelector()
+
+const container = document.querySelector("div");
+
+console.log(container);
+
+// querySelectorAll()
+
+const containers = document.querySelectorAll("div");
+
+console.log(containers);
