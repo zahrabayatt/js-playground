7daf2745d0b69698b171f7d74ea96457cf73b000
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-07-31 13:59:59 +0330

    Modifying text in the DOM

--- src/main.js
+++ src/main.js
@@ -7,3 +7,13 @@ const li = document.createElement("li");
 // Adding Element
 
 ul.append(li);
+
+// Modifying the text
+
+const firstListItem = document.querySelector(".list-items");
+
+console.log(firstListItem.innerText);
+console.log(firstListItem.textContent);
+console.log(firstListItem.innerHTML);
+
+li.innerText = "X-men";
