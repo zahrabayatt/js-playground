cf8c55d48b99843730cf2a3b04eb8b8528cf9e6f
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-07-31 14:06:29 +0330

    Modifying attributes & classes in the DOM

--- src/main.js
+++ src/main.js
@@ -17,3 +17,19 @@ console.log(firstListItem.textContent);
 console.log(firstListItem.innerHTML);
 
 li.innerText = "X-men";
+
+// Modifying Attributes
+
+li.setAttribute("id", "main-heading");
+li.removeAttribute("id");
+
+const title = document.querySelector("#main-heading");
+
+console.log(title.getAttribute("id"));
+
+// Modifying Classes
+
+li.classList.add("list-items");
+li.classList.remove("list-items");
+
+console.log(li.classList.contains("list-items"));
