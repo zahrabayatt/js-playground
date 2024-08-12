12fc8a50397f00a5a829ea2c532349270338c6ac
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-12 21:31:20 +0330

    Project: Accordion

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
 // Variables
 
-let openBtn = document.getElementById("open-btn");
-let modalContainer = document.getElementById("modal-container");
-let closeBtn = document.getElementById("close-btn");
+const accordion = document.getElementsByClassName("content-container");
 
-// Event Listeners
-
-openBtn.addEventListener("click", function () {
-  modalContainer.style.display = "block";
-});
-
-closeBtn.addEventListener("click", function () {
-  modalContainer.style.display = "none";
-});
-
-window.addEventListener("click", function (e) {
-  if (e.target === modalContainer) {
-    modalContainer.style.display = "none";
-  }
-});
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
