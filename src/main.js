af1d5ade6ae4f5e4acef2b770e17c3ec65f61056
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-12 22:26:30 +0330

    Project: Stop-Watch

--- src/main.js
+++ src/main.js
@@ -1,9 +1,74 @@
-// Variables
+// Variables for buttons
 
-const accordion = document.getElementsByClassName("content-container");
+const startStopBtn = document.querySelector("#startStopBtn");
+const restBtn = document.querySelector("#restBtn");
 
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-  });
+// Variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Variables for leading zero
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// Variables for set interval & timer status
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+// Stop Watch Function
+
+function stopWatch() {
+  seconds++;
+
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  leadingSeconds = seconds < 10 ? "0" + seconds.toString() : seconds;
+
+  leadingMinutes = minutes < 10 ? "0" + minutes.toString() : minutes;
+
+  leadingHours = hours < 10 ? "0" + hours.toString() : hours;
+
+  let displayTimer = (document.getElementById("timer").innerText =
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
+
+//window.setInterval(stopWatch, 1000);
+startStopBtn.addEventListener("click", function () {
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(stopWatch, 1000);
+
+    document.getElementById("startStopBtn").innerHTML =
+      '<i class="fa-solid fa-pause" id="pause"></i>';
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    document.getElementById("startStopBtn").innerHTML =
+      '<i class="fa-solid fa-play" id="play"></i>';
+    timerStatus = "stopped";
+  }
+});
+
+restBtn.addEventListener("click", function () {
+  window.clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  document.getElementById("timer").innerHTML = "00:00:00";
+  document.getElementById("startStopBtn").innerHTML =
+    '<i class="fa-solid fa-play" id="play"></i>';
+  timerStatus = "stopped";
+});
